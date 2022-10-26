1129b588902b812dd0a67e100d90731a215ab693
LaYt <laytya@gmail.com>
2022-10-26 15:30:18 +0700

    some fixes

--- AceGUI-3.0/widgets/AceGUIContainer-SimpleGroup.lua
+++ AceGUI-3.0/widgets/AceGUIContainer-SimpleGroup.lua
@@ -51,7 +51,7 @@ local function Constructor()
 
 	--Container Support
 	local content = CreateFrame("Frame", nil, frame)
-	content:SetPoint("TOPLEFT", 0,0)
+	content:SetPoint("TOPLEFT",0,0)
 	content:SetPoint("BOTTOMRIGHT",0,0)
 
 	local widget = {
